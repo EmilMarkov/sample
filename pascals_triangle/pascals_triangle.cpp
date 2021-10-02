7358d27c9ca528353aabebc3b602108540a8a431
EmilMarkov <markovemil2@mail.ru>
2021-10-02 11:11:04 +0300

    Create pascals_triangle.cpp

---
+++ pascals_triangle/pascals_triangle.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int faсtorial(int n)
+{
+	int i, x;
+	x = 1;
+	for (i = 1; i <= n; i++)
+	{
+		x *= i;
+	};
+
+	return x;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << "1" << "\n";
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << (faсtorial(i) / (faсtorial(j) * faсtorial(i - j))) << " ";
+		};
+		cout << "\n";
+	};
+	return 0;
+}
